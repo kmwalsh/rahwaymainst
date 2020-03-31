50aece62a05a143d84d6d22487619e4118cadb81
Kate <kmw@katemwalsh.com>
2020-03-31 00:14:25 -0400

    purgecss - 840kb to 20kb css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
 
 /*
  |--------------------------------------------------------------------------
@@ -18,4 +19,5 @@ mix.js('resources/js/app.js', 'public/js')
    .options({
       processCssUrls: false,
       postCss: [ tailwindcss('tailwind.config.js') ],
-});
+   })
+   .purgeCss();
